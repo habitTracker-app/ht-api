7f133f8cd82591e5aebf543bf90a09627ce75ac1
mclaramarinho <marinho.claramb@gmail.com>
2024-07-29 22:16:36 -0300

    FEAT: add friend body model

---
+++ HTApi/Models/ActionModels/AddFriend.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HTApi.Models.ActionModels
+{
+    public class AddFriend
+    {
+        [Required]
+        public int RequesterUuid { get; set; }
+
+        [Required]
+        public int TargetUuid { get; set; }
+    }
+}
