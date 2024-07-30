51516e3e77f99535892706711c8e8ac0d1181c33
mclaramarinho <marinho.claramb@gmail.com>
2024-07-29 22:18:14 -0300

    FEAT: get user by jwt token

--- HTApi/Services/TokenService.cs
+++ HTApi/Services/TokenService.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using HTApi.DTOs;
+using HTAPI.Data;
+using HTAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
@@ -8,14 +12,21 @@ namespace HTApi.Services
     public interface ITokenService
     {
         string GenerateJwtToken(string email, bool persist, int uuid, string identityUserId);
+        User GetUserByJWT();
     }
     public class TokenService : ITokenService
     {
+        private readonly IHttpContextAccessor? _http;
+
         private readonly IConfiguration _config;
 
-        public TokenService(IConfiguration config)
+        private readonly AppDbContext _db;
+
+        public TokenService(IConfiguration config, IServiceProvider sp)
         {
             _config = config;
+            _http = sp.GetService<IHttpContextAccessor>();
+            _db = sp.GetRequiredService<AppDbContext>();
         }
 
         public string GenerateJwtToken(string email, bool persist, int uuid, string identityUserId)
@@ -49,6 +60,33 @@ namespace HTApi.Services
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
             return tokenHandler.WriteToken(token);
+        }
+        
+        public User GetUserByJWT()
+        {
+            ClaimsIdentity claimsIdentity = _http.HttpContext.User.Identity as ClaimsIdentity;
+            var claims = claimsIdentity?.Claims;
+
+            if (claims == null) { throw new Exception("No claims found in jwt."); }
+
+            UserClaimsDTO userClaimsDTO = new UserClaimsDTO
+            {
+                Email = claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value,
+                Id = claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value,
+                UUID = claims.FirstOrDefault(c => c.Type == ClaimTypes.SerialNumber)?.Value
+            };
+
+            Console.WriteLine(userClaimsDTO.Email);
+            Console.WriteLine(userClaimsDTO.UUID);
+            Console.WriteLine(userClaimsDTO.Id);
+
+            User? requester = _db.Users.FirstOrDefault(u => (u.Id == userClaimsDTO.Id) &&
+                                                            (u.UUID.ToString() == userClaimsDTO.UUID.ToString()) &&
+                                                            (u.Email == userClaimsDTO.Email));
+
+            if (requester == null) { throw new Exception("400 - This user does not exist."); }
+
+            return requester;
         }
     }
 }
