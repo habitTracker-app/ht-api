78bc6ac5cddabeedef25cfcc48dbb5f3ce51fbf2
mclaramarinho <marinho.claramb@gmail.com>
2024-07-30 21:10:40 -0300

    FEAT: Model for updating user info body

---
+++ HTApi/Models/ActionModels/UpdateUserInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HTApi.Models.ActionModels
+{
+    public class UpdateUserInfo
+    {
+        public string Email {  get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        public int CountryId { get; set; }
+
+        public int GenderId { get; set; }
+    }
+}
